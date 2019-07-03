0880bf1dc2a7adb8187be24da116ac01aefd99af
heese.sophia@gmail.com <heese.sophia@gmail.com>
2019-07-03 22:28:03 +0200

    DAO Entreprise et Contrat

--- BiblioMetierDLL/DAL/ContratDAO.cs
+++ BiblioMetierDLL/DAL/ContratDAO.cs
@@ -1,4 +1,4 @@
-﻿using BiblioMetierDLL;
+﻿using BiblioMetierDLL.BLL;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
--- BiblioMetierDLL/DAL/EntrepriseDAO.cs
+++ BiblioMetierDLL/DAL/EntrepriseDAO.cs
@@ -1,4 +1,4 @@
-﻿using BiblioMetierDLL;
+﻿using BiblioMetierDLL.BLL;
 using ProjetMutuelle.Models;
 using System;
 using System.Collections.Generic;
